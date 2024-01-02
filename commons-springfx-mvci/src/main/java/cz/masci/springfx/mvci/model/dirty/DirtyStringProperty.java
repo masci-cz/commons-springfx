74828aa01959d46a1342a60158a00834d55292ad
Daniel Masek <daniel@masci.cz>
2024-01-02 17:20:11 +0100

    DD-45 Add license information to some files

--- commons-springfx-mvci/src/main/java/cz/masci/springfx/mvci/model/dirty/DirtyStringProperty.java
+++ commons-springfx-mvci/src/main/java/cz/masci/springfx/mvci/model/dirty/DirtyStringProperty.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2023
+ *
+ * This file is part of commons-springfx library.
+ *
+ * commons-springfx library is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free
+ *  Software Foundation, either version 3 of the License, or (at your option)
+ *   any later version.
+ *
+ * commons-springfx library is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *   FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+ *    License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ *  along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package cz.masci.springfx.mvci.model.dirty;
 
 import java.util.Objects;
@@ -12,7 +31,6 @@ import javafx.beans.value.WeakChangeListener;
 import org.jetbrains.annotations.NotNull;
 import org.nield.dirtyfx.tracking.DirtyProperty;
 
-// TODO: Create DirtyObservableList where dirty means that one of items is dirty
 public class DirtyStringProperty extends StringProperty implements DirtyProperty {
 
   private final StringProperty originalValue;
